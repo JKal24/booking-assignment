9dad2c595b39f3e1735e4a5db707dd4cd8969d64
JKal24 <jkalirai2499@gmail.com>
2022-02-07 19:46:27 -0500

    removing unnecessary imports

--- src/pages/components/header/Header.js
+++ src/pages/components/header/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import HeaderBackground from '../../../assets/header-background.png';
 import Logo from '../../../assets/logo.png';
 import './Header.css';
